eea9bfa82c43200534ab80a15a8690c0b6c5dab1
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-17 22:45:27 +0300

    feat: add layouts

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-import Head from "next/head";
-
+import Layout from "../components/layout/Layout";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}>Home</div>;
+  return (
+    <Layout>
+      <p> main layout</p>
+    </Layout>
+  );
 }
